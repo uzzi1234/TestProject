74bf1419401144a0770cc217beda1aafd701b84b
Uzair <uzair.omar@accenture.com>
2018-12-03 15:15:22 +0200

    travis

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -1,9 +1,6 @@
-#! /bin/sh
+#!/bin/sh
 
-# Example build script for Unity3D project. See the entire example: https://github.com/JonathanPorta/ci-build
-
-# Change this the name of your project. This will be the name of the final executables as well.
-project="ci-build"
+project="TestProject"
 
 echo "Attempting to build $project for Windows"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
@@ -37,3 +34,9 @@ echo "Attempting to build $project for Linux"
 
 echo 'Logs from build'
 cat $(pwd)/unity.log
+
+
+echo 'Attempting to zip builds'
+zip -r $(pwd)/Build/linux.zip $(pwd)/Build/linux/
+zip -r $(pwd)/Build/mac.zip $(pwd)/Build/osx/
+zip -r $(pwd)/Build/windows.zip $(pwd)/Build/windows/
